7ee0a101cac6dcecf7da54a3a4fbc06ef52348a7
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-06-04 23:49:48 +0600

    planet list created

--- src/screens/home.js
+++ src/screens/home.js
@@ -4,17 +4,21 @@ import Text from '../components/text/text';
 import PlanetHeader from '../components/planet-header';
 import { colors } from '../theme/colors';
 import { PLANET_LIST } from '../data/planet-list';
+import { spacing } from '../theme/spacing';
 
 export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <PlanetHeader />
       <FlatList
+          contentContainerStyle={styles.list}
           data={PLANET_LIST}
-          renderItem= {({item, index}) =>{
+          keyExtractor={(item) => item.name}
+          renderItem= {({item}) =>{
             return(
-              <View>
-                  <Text>
+              <View style={styles.item}>
+                  <View style={[styles.circle, {backgroundColor: item.color}]} />
+                  <Text preset="h4" style={styles.itemName}>
                       {item.name}
                   </Text>
               </View>
@@ -30,5 +34,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.black
+  },
+
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+  },
+
+  item:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: spacing[4],
+  },
+
+  itemName:{
+    textTransform: 'uppercase',
+    marginLeft: spacing[4],
+  },
+
+  list:{
+    padding: spacing[5],
   }
 })
