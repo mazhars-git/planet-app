da84043b24cecddddc5ddd5a43ff8b07e3094394
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-06-04 19:40:27 +0600

    planet data rendared

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,12 +1,34 @@
-import { SafeAreaView } from 'react-native'
+import { View, FlatList, SafeAreaView, StyleSheet } from 'react-native'
 import React from 'react'
 import Text from '../components/text/text';
 import PlanetHeader from '../components/planet-header';
+import { colors } from '../theme/colors';
+import { PLANET_LIST } from '../data/planet-list';
 
 export default function Home() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <PlanetHeader />
+      <FlatList
+          data={PLANET_LIST}
+          renderItem= {({item, index}) =>{
+            return(
+              <View>
+                  <Text>
+                      {item.name}
+                  </Text>
+              </View>
+            );
+          }}
+      />
     </SafeAreaView>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.black
+  }
+})
