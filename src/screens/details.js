5553da433582e641b7ae24d498b136ce3ab2c01b
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-06-22 10:27:33 +0600

    svg done

--- src/screens/details.js
+++ src/screens/details.js
@@ -1,15 +1,44 @@
-import { View, SafeAreaView, StyleSheet } from 'react-native'
+import { View, SafeAreaView, StyleSheet, ScrollView } from 'react-native'
 import React from 'react'
 import Text from '../components/text/text'
 import PlanetHeader from '../components/planet-header'
 import { colors } from '../theme/colors'
+import { MercurySvg, JupiterSvg, NeptuneSvg, MarsSvg, EarthSvg, SaturnSvg, UranusSvg, VenusSvg } from '../svg';
+import { spacing } from './../theme/spacing';
 
 export default function Details({navigation, route}) {
   const planet = route.params.planet;
+  const {name} = planet;
   console.log(planet);
+
+  const renderImage = (name) =>{
+    switch (name) {
+      case "mercury":
+        return <MercurySvg />;
+      case "jupiter":
+        return <JupiterSvg />;
+      case "neptune":
+        return <NeptuneSvg />;
+      case "mars":
+        return <MarsSvg />;
+      case "earth":
+        return <EarthSvg />;
+      case "saturn":
+        return <SaturnSvg />;
+      case "uranus":
+        return <UranusSvg />;
+      case "venus":
+        return <VenusSvg />;
+    }
+  }  
   return (
     <SafeAreaView style= {styles.container}>
         <PlanetHeader backBtn={true} />
+        <ScrollView>
+            <View style= {styles.imageView}>
+                {renderImage(name)}
+            </View>
+        </ScrollView>
     </SafeAreaView>
   )
 }
@@ -18,5 +47,10 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: colors.black,
 
+    },
+    imageView: {
+      padding: spacing[5],
+      alignItems: 'center',
+      justifyContent: 'center',
     }
 })
